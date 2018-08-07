9c573457662c9d29f4ad570431d98479d98d00a7
BogdanFloris <bogdan.floris@gmail.com>
2018-08-07 23:09:50 +0300

    initial blank game board done

---
+++ board.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"log"
+
+	"github.com/go-gl/gl/v4.1-core/gl"
+	"github.com/go-gl/glfw/v3.2/glfw"
+)
+
+const (
+	width  = 500 // width of the game board
+	height = 500 // height of the game board
+)
+
+var (
+	square = []float32{
+		-0.5, 0.5, 0,
+		-0.5, -0.5, 0,
+		0.5, -0.5, 0,
+
+		-0.5, 0.5, 0,
+		0.5, 0.5, 0,
+		0.5, -0.5, 0,
+	}
+)
+
+type cell struct {
+	drawable uint32
+
+	white bool
+
+	x int
+	y int
+}
+
+func (c *cell) draw() {
+	if !c.white {
+		return
+	}
+}
+
+// initGlfw initializes glfw and returns a Window to use.
+func initGlfw() *glfw.Window {
+	if err := glfw.Init(); err != nil {
+		panic(err)
+	}
+
+	glfw.WindowHint(glfw.Resizable, glfw.False)
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+
+	window, err := glfw.CreateWindow(width, height, "N-Queens Problem", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+	window.MakeContextCurrent()
+
+	return window
+}
+
+// initOpenGL initializes OpenGL and returns an intiialized program.
+func initOpenGL() uint32 {
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+	version := gl.GoStr(gl.GetString(gl.VERSION))
+	log.Println("OpenGL version", version)
+
+	prog := gl.CreateProgram()
+	gl.LinkProgram(prog)
+	return prog
+}
+
+func draw(window *glfw.Window, program uint32) {
+	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+	gl.UseProgram(program)
+
+	glfw.PollEvents()
+	window.SwapBuffers()
+}
--- main.go
+++ main.go
@@ -3,12 +3,24 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"runtime"
+
+	"github.com/go-gl/glfw/v3.2/glfw"
 )
 
 // N flag: the number of queens and the size of the board
 var N = flag.Int("N", 4, "the number of queens and the size of the board")
 
 func main() {
-	fmt.Println("Hello, World!")
+	runtime.LockOSThread()
+
+	window := initGlfw()
+	defer glfw.Terminate()
+
+	program := initOpenGL()
+
+	for !window.ShouldClose() {
+		// TODO
+		draw(window, program)
+	}
 }
